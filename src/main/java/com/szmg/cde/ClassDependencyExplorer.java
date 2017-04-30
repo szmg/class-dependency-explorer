adc03cdb1b8387845b3820c52b3e7ed8e5e7806c
Mate Szvoboda <mate.szvoboda@gmail.com>
2017-04-30 14:12:56 +0100

    first one

---
+++ src/main/java/com/szmg/cde/ClassDependencyExplorer.java
@@ -0,0 +1,13 @@
+package com.szmg.cde;
+
+public class ClassDependencyExplorer {
+
+    public static void main(String args[]) {
+        System.out.println("Hello!");
+
+        new InputConfig("/Users/mateszvoboda/gitexp/componentsvc/web/target/component-svc-web-58.0-SNAPSHOT.war");
+
+
+    }
+
+}
---
+++ src/main/java/com/szmg/cde/InputConfig.java
@@ -0,0 +1,15 @@
+package com.szmg.cde;
+
+public class InputConfig {
+
+    private String path;
+
+    public InputConfig(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
